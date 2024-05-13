df6c39a433e22246af1a7dd9dec3b4218a095d2b
Hay88Git <myvercel2024@gmail.com>
2024-05-13 10:25:50 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,47 @@
+function ShowNav() {
+    document.getElementById("nav").style.width = "80%";
+    document.getElementById("close").style.display = "block";
+}
+function HideNav() {
+    document.getElementById("nav").style.width = "0%";
+    document.getElementById("close").style.display = "none";
+}
+
+// Slide
+var newImageSlide = [
+    "./img/1.jpg",
+    "./img/2.jpg"
+];
+var slide = document.querySelectorAll(".main_slide img");
+slide.forEach(function(img, index) {
+    img.src = newImageSlide[index];
+});
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 500,
+    center: true,
+    items: 1,
+    margin: 10,
+})
+
+// Change Pic
+var newImageSources = [
+    "./img/thumnail/1.png",
+    "./img/thumnail/2.png",
+    "./img/thumnail/3.png",
+    "./img/thumnail/4.png",
+    "./img/thumnail/5.png",
+    "./img/thumnail/6.png",
+];
+var images = document.querySelectorAll(".pic img");
+images.forEach(function(img, index) {
+    img.src = newImageSources[index];
+});
+
+var newAdr = "./img/1.png";
+var imgAdr = document.querySelectorAll(".adr img");
+imgAdr.forEach(function(img) {
+    img.src = newAdr;
+});
